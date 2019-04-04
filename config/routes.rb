02136c9943f25102bf988a4173ace03089ba1ee2
silk-k <silk@kasaharafuminoMacBook-Pro.local>
2019-04-04 19:05:12 +0900

    ルートの設定とグループコントローラにアクションを定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
  get 'messages'   => 'messages#index'
  resources :users, only: [:edit, :update]
+ resources :groups, only: [:new, :create, :edit, :update] do
 end
