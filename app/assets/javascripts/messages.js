30ac3f21c0e69547172d51ad4593a6186a0e5303
silk-k <silk@kasaharafuminoMacBook-Pro.local>
2019-04-10 16:20:18 +0900

    formのresetメソッドに変更

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -34,7 +34,7 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.form__message').val('');
+      $('form')[0].reset();
       $('.form__submit').prop('disabled', false);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
