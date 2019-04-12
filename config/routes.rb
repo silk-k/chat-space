960544f94a23a23a8841296f26cc87657f985f90
silk-k <silk@kasaharafuminoMacBook-Pro.local>
2019-04-12 15:50:58 +0900

    ルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, only: [:edit, :update]
-
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
   resources :messages, only: [:index, :create]
-  end
-  rout 'users#index'
-   resources :users, only: [:index] do
-    collection do
-      get  :ajax_user_list
-    end
   end
 end
