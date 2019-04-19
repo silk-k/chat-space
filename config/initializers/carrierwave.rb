bd2b0626677327fd053b015af110c5a897e7ed7a
silk-k <aomine1010@gmail.com>
2019-04-19 17:38:13 +0900

    追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'silkchatspace'
-  config.asset_host = 'https://ap-northeast-1.amazonaws.com/silkchatspace'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/silkchatspace'
 end
